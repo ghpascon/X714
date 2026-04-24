bf708bd782c53adb42812b6f503cfe1176157ba7
Gabriel Henrique Pascon <gh.pascon@gmail.com>
2026-04-24 17:17:42 -0300

    patch - trim cmd before treat

--- app/myserial_check/_main.h
+++ app/myserial_check/_main.h
@@ -220,6 +220,7 @@ private:
 public:
     void check_commands(String cmd)
     {
+        cmd.trim();
         if (cmd.length() == 0)
             return;
         // ================= Comandos Gerais =================
