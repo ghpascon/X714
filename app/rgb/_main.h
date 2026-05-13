e0129a2c9c760c383d7d0e9043f00410c7cd1b3e
Gabriel Henrique Pascon <gh.pascon@gmail.com>
2026-05-13 15:52:56 -0300

    patch - External led

--- app/rgb/_main.h
+++ app/rgb/_main.h
@@ -7,28 +7,61 @@ public:
 	void setup()
 	{
 		FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+		pinMode(EXTERNAL_LED_RED_PIN, OUTPUT);
+		pinMode(EXTERNAL_LED_GREEN_PIN, OUTPUT);
+		pinMode(EXTERNAL_LED_BLUE_PIN, OUTPUT);
 	}
 
 	void state()
 	{
+		static String last_state = "";
+		String state = String(setup_done) + String(read_on) + String(eth_connected);
+		if (state == last_state)
+			return;
+		last_state = state;
 		byte led_brigthness = 0x50;
 
 		// SETUP
 		if (!setup_done)
+		{
 			leds[0] = CRGB(led_brigthness, 0x00, 0x00);
+			digitalWrite(EXTERNAL_LED_RED_PIN, LOW);
+			digitalWrite(EXTERNAL_LED_GREEN_PIN, HIGH);
+			digitalWrite(EXTERNAL_LED_BLUE_PIN, HIGH);
+		}
 
 		// IDLE
 		else if (!read_on)
 			if (eth_connected)
+			{
 				leds[0] = CRGB(0x00, 0x00, led_brigthness);
+				digitalWrite(EXTERNAL_LED_RED_PIN, HIGH);
+				digitalWrite(EXTERNAL_LED_GREEN_PIN, HIGH);
+				digitalWrite(EXTERNAL_LED_BLUE_PIN, LOW);
+			}
 			else
+			{
 				leds[0] = CRGB(led_brigthness, led_brigthness, 0x00);
+				digitalWrite(EXTERNAL_LED_RED_PIN, LOW);
+				digitalWrite(EXTERNAL_LED_GREEN_PIN, LOW);
+				digitalWrite(EXTERNAL_LED_BLUE_PIN, HIGH);
+			}
 
 		// READING
 		else if (eth_connected)
+		{
 			leds[0] = CRGB(0x00, led_brigthness, led_brigthness);
+			digitalWrite(EXTERNAL_LED_RED_PIN, HIGH);
+			digitalWrite(EXTERNAL_LED_GREEN_PIN, LOW);
+			digitalWrite(EXTERNAL_LED_BLUE_PIN, LOW);
+		}
 		else
+		{
 			leds[0] = CRGB(0x00, led_brigthness, 0x00);
+			digitalWrite(EXTERNAL_LED_RED_PIN, HIGH);
+			digitalWrite(EXTERNAL_LED_GREEN_PIN, LOW);
+			digitalWrite(EXTERNAL_LED_BLUE_PIN, HIGH);
+		}
 
 		FastLED.show();
 	}
--- pins.h
+++ pins.h
@@ -19,3 +19,7 @@
 
 #define buzzer_pin 4
 #define indicator_pin 42
+
+#define EXTERNAL_LED_RED_PIN 6
+#define EXTERNAL_LED_GREEN_PIN 5
+#define EXTERNAL_LED_BLUE_PIN 7
